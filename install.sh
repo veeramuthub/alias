9c24d65e2697b347308eb85420f8a1c2f0e748fe
Veeramuthu <veeramuthub@gmail.com>
2023-08-15 13:29:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/veeramuthub/alias/main/.veera.sh ~/
+wget https://raw.githubusercontent.com/veeramuthub/alias/main/.veera.sh -O ~/.veera.sh
 echo 'source ~/.veera.sh' >> ~/.bashrc
